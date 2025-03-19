cefd2330e5fe6d252bcbb863cfe01435ddb96eca
Brenna Meyer <brennameyer9@gmail.com>
2025-03-19 16:42:36 -0400

    Add project files.

---
+++ Chapter 5 Exercise 14 and 25 - Student Lineup.cpp
@@ -0,0 +1,28 @@
+// Chapter 5 Exercise 14 and 25 - Student Lineup.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//// Programmer: Brenna Meyer 
+// Date: March 19th, 2025
+// Requirements: A teacher has asked all her students to line up according to their first name.
+//For example, in one class Amy will be at the front of the line, and Yolanda will be at the end.
+// Write a program that prompts the user to enter the number of students in the class, then loops to read that many names.
+// Once all the names have been read, it reports which student would be at the front of the line and
+// which one would be at the end of the line.You may assume that no two students have the same name.
+// Input Validation : Do not accept a number less than 1 or greater than 25 for the number of students.
+
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
